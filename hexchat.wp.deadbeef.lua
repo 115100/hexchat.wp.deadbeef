66ef58ed2642752a39c2933177cf71b87752ccd1
115100 <git@three-sigma.org>
2021-05-01 21:06:29 +0100

    hexchat.wp.deadbeef: Rewrite in lua

---
+++ hexchat.wp.deadbeef.lua
@@ -0,0 +1,24 @@
+hexchat.register("DEADBEEF", "1", "DeaDBeeF currently playing script")
+
+local function get_np()
+    local handle =
+        assert(
+            io.popen(
+                table.concat(
+                    {
+                        "deadbeef",
+                        "--noplaying",
+                        '"%a - %t 「%b」"'
+                    },
+                    " "
+                )
+            )
+        )
+    local res = string.gsub(assert(handle:read("*a")), "\n", "")
+    handle:close()
+    hexchat.command("SAY np: " .. res)
+    return hexchat.EAT_ALL
+end
+
+hexchat.hook_command("wp", get_np, '"/wp" to display currently playing DeaDBeeF track')
+hexchat.print("hexchat.wp.deadbeef.lua loaded")
